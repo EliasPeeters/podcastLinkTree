f02c4e0b78a2f58ff28c9e630faf040e49db5ea6
Elias Peeters <Elias.peeters@otto.de>
2021-08-29 10:38:49 +0200

    Added logger boolean

--- logger.js
+++ logger.js
@@ -1,18 +1,20 @@
 function log(req, url, id) {
-    if (url == undefined) {
-        url = req.route.path
+    if (process.env.LOGGER == 'true') {
+        if (url == undefined) {
+            url = req.route.path
+        }
+        let data = {
+            table: 'logging',
+            log_dateTime: new Date(),
+            log_uuid: id,
+            log_url: url
+        }
+        console.log(data)
+        let query = connection.createQueryStringFromObject(data);
+        connection.query(query)
+        let timeStamp = new Date();
+        console.log(query)
     }
-    let data = {
-        table: 'logging',
-        log_dateTime: new Date(),
-        log_uuid: id,
-        log_url: url
-    }
-    console.log(data)
-    let query = connection.createQueryStringFromObject(data);
-    connection.query(query)
-    let timeStamp = new Date();
-    console.log(query)
 }
 
 module.exports = {log}
